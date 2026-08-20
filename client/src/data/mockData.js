315d2946f406ad3f51003420ee58dcad6adfbc22
gayathma2004-afk <gayathma2004@icloud.com>
2026-08-20 21:13:11 +0530

    Add Column component and mock data

---
+++ client/src/data/mockData.js
@@ -0,0 +1,26 @@
+ const mockData = {
+  columns: [
+    {
+      id: "todo",
+      title: "To Do",
+      tasks: [
+        { id: "1", title: "Design wireframes" },
+        { id: "2", title: "Set up repo" },
+      ],
+    },
+    {
+      id: "doing",
+      title: "Doing",
+      tasks: [
+        { id: "3", title: "Build Board component" },
+      ],
+    },
+    {
+      id: "done",
+      title: "Done",
+      tasks: [],
+    },
+  ],
+};
+
+export default mockData;
