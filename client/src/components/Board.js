f4a42dc582f776631bbfe2657c7b08d39ae2dd95
Imadi Rajapaksha <rajapakshaimadi25@gmail.com>
2026-08-20 18:50:55 +0530

    Add Board component

---
+++ client/src/components/Board.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Board() {
+  return (
+    <div className="board">
+      <h1>SyncBoard</h1>
+      <div className="board-columns">
+        {/* Column components will go here */}
+        <p>Columns will be added by teammates</p>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
